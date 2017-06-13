ec12159bda232b8adbe235b27b9cb3f3f2912315
Andrew Chow <achow101-github@achow101.com>
2017-06-12 18:53:46 -0700

    [rpc] Create getaddressinfo RPC and deprecate parts of validateaddress
    Moves the parts of validateaddress which require the wallet into getaddressinfo
    which is part of the wallet RPCs. Mark those parts of validateaddress which
    require the wallet as deprecated.
    
    validateaddress will call getaddressinfo for the data that both share
    for right now.

--- src/rpc/util.h
+++ src/rpc/util.h
@@ -2,10 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <pubkey.h>
 #include <script/standard.h>
 #include <univalue.h>
-#include <utilstrencodings.h>
 
 #include <boost/variant/static_visitor.hpp>
 
