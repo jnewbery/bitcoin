2dbf4fddd2a2b47e7fe44f561a70ea33faadf1e5
John Newbery <john@johnnewbery.com>
2017-10-23 10:53:29 -0400

    [zmq] [tests] Add multiwallet zmq test

--- test/functional/zmq_test.py
+++ test/functional/zmq_test.py
@@ -8,11 +8,12 @@ import os
 import struct
 
 from test_framework.test_framework import BitcoinTestFramework, SkipTest
-from test_framework.util import (assert_equal,
-                                 assert_not_equal,
-                                 bytes_to_hex_str,
-                                 hash256,
-                                )
+from test_framework.util import (
+    assert_equal,
+    assert_not_equal,
+    bytes_to_hex_str,
+    hash256,
+)
 
 class ZMQSubscriber:
     def __init__(self, socket, topic):
@@ -23,7 +24,7 @@ class ZMQSubscriber:
         import zmq
         self.socket.setsockopt(zmq.SUBSCRIBE, self.topic)
 
-    def receive(self, specific_topic = None):
+    def receive(self, specific_topic=None):
         expected_topic = specific_topic if specific_topic else self.topic
 
         topic, body, seq = self.socket.recv_multipart()
@@ -73,9 +74,16 @@ class ZMQTest (BitcoinTestFramework):
         self.hashwallettx = ZMQSubscriber(socket, b"hashwallettx")
         self.rawwallettx = ZMQSubscriber(socket, b"rawwallettx")
 
-        self.extra_args = [["-zmqpub%s=%s" % (sub.topic.decode(), address) for sub in [self.hashblock, self.hashtx, self.rawblock, self.rawtx, self.hashwallettx, self.rawwallettx]], []]
+        zmq_extra_args = ["-zmqpub%s=%s" % (sub.topic.decode(), address) for sub in [self.hashblock, self.hashtx, self.rawblock, self.rawtx, self.hashwallettx, self.rawwallettx]]
+        # Make sure that zmq notifications work for multiwallet
+        wallet_extra_args = ["-wallet=w1", "-wallet=w2"]
+        self.extra_args = [zmq_extra_args + wallet_extra_args, []]
         self.add_nodes(self.num_nodes, self.extra_args)
         self.start_nodes()
+
+        # Get references to the wallets
+        self.w1 = self.nodes[0].get_wallet_rpc("w1")
+        self.w2 = self.nodes[0].get_wallet_rpc("w2")
 
     def run_test(self):
         try:
@@ -88,7 +96,7 @@ class ZMQTest (BitcoinTestFramework):
     def _zmq_test(self):
         num_blocks = 5
         self.log.info("Generate %(n)d blocks (and %(n)d coinbase txes)" % {"n": num_blocks})
-        genhashes = self.nodes[0].generate(num_blocks)
+        genhashes = self.w1.generate(num_blocks)
         self.sync_all()
 
         for x in range(num_blocks):
@@ -114,21 +122,23 @@ class ZMQTest (BitcoinTestFramework):
             block = self.rawblock.receive()
             assert_equal(genhashes[x], bytes_to_hex_str(hash256(block[:80])))
 
-        self.log.info("Wait for tx from second node")
-        payment_txid = self.nodes[1].sendtoaddress(self.nodes[0].getnewaddress(), 1.0)
-        self.sync_all()
+        self.log.info("Wait for txs from second node")
+        for i, wallet in enumerate([self.w1, self.w2], start=1):
+            payment_txid = self.nodes[1].sendtoaddress(wallet.getnewaddress(), 1.0)
+            self.sync_all()
 
-        # Should receive the broadcasted txid.
-        txid = self.hashtx.receive()
-        assert_equal(payment_txid, bytes_to_hex_str(txid))
+            # Should receive the broadcasted txid.
+            txid = self.hashtx.receive()
+            assert_equal(payment_txid, bytes_to_hex_str(txid))
 
-        # Should receive the broadcasted raw transaction.
-        hex = self.rawtx.receive()
-        assert_equal(payment_txid, bytes_to_hex_str(hash256(hex)))
+            # Should receive the broadcasted raw transaction.
+            hex = self.rawtx.receive()
+            assert_equal(payment_txid, bytes_to_hex_str(hash256(hex)))
 
-        wallettxid = self.hashwallettx.receive(b"hashwallettx-mempool")
-        wallethex = self.rawwallettx.receive(b"rawwallettx-mempool")
-        assert_equal(hash256(wallethex), wallettxid)
+            self.log.info("Receive transaction to wallet %s" % i)
+            wallettxid = self.hashwallettx.receive(b"hashwallettx-mempool")
+            wallethex = self.rawwallettx.receive(b"rawwallettx-mempool")
+            assert_equal(hash256(wallethex), wallettxid)
 
 if __name__ == '__main__':
     ZMQTest().main()
