7a50abed8fc0a43d4790a5c8ebe1c0142d2e7587
John Newbery <john@johnnewbery.com>
2017-12-04 11:43:34 -0500

    [tests] Add tests for new getaddressinfo RPC

--- test/functional/deprecated_rpc.py
+++ test/functional/deprecated_rpc.py
@@ -10,7 +10,7 @@ class DeprecatedRpcTest(BitcoinTestFramework):
     def set_test_params(self):
         self.num_nodes = 2
         self.setup_clean_chain = True
-        self.extra_args = [[], ["-deprecatedrpc=estimatefee"]]
+        self.extra_args = [[], ["-deprecatedrpc=estimatefee", "-deprecatedrpc=validateaddress"]]
 
     def run_test(self):
         self.log.info("estimatefee: Shows deprecated message")
@@ -18,6 +18,13 @@ class DeprecatedRpcTest(BitcoinTestFramework):
 
         self.log.info("Using -deprecatedrpc=estimatefee bypasses the error")
         self.nodes[1].estimatefee(1)
+
+        self.log.info("Test validateaddress deprecation")
+        SOME_ADDRESS = "mnvGjUy3NMj67yJ6gkK5o9e5RS33Z2Vqcu" # This is just some random address to pass as a parameter to validateaddress
+        dep_validate_address = self.nodes[0].validateaddress(SOME_ADDRESS)
+        assert "ismine" not in dep_validate_address
+        not_dep_val = self.nodes[1].validateaddress(SOME_ADDRESS)
+        assert "ismine" in not_dep_val
 
 if __name__ == '__main__':
     DeprecatedRpcTest().main()
