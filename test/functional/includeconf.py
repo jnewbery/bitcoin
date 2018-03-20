968c9158f0bffac8c91282f1789a5a1808ed13b9
John Newbery <john@johnnewbery.com>
2018-03-20 11:29:04 -0400

    suggested test changes

--- test/functional/includeconf.py
+++ test/functional/includeconf.py
@@ -1,38 +1,71 @@
 #!/usr/bin/env python3
-# Copyright (c) 2017 The Bitcoin Core developers
+# Copyright (c) 2018 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
-"""
-Tests the includeconf directive
+"""Tests the includeconf argument
 
-Create an additional configuration file that is loaded from the main
-bitcoin.conf via includeconf (done in setup_chain). We check that:
-1. The files (bitcoin.conf and relative.conf) are indeed loaded.
-2. The load ordering is correct (bitcoin.conf, then relative).
-"""
+Verify that:
 
+1. adding includeconf to the configuration file causes the includeconf
+   file to be loaded in the correct order.
+2. includeconf cannot be used as a command line argument.
+3. includeconf cannot be used recursively (ie includeconf can only
+   be used from the base config file).
+4. multiple includeconf arguments can be specified in the main config
+   file (Currently fails).
+"""
 import os
+
 from test_framework.test_framework import BitcoinTestFramework
 
 class IncludeConfTest(BitcoinTestFramework):
     def setup_chain(self):
         super().setup_chain()
-        # Create additional config file
+        # Create additional config files
         # - tmpdir/node0/relative.conf
         with open(os.path.join(self.options.tmpdir + "/node0", "relative.conf"), "w", encoding="utf8") as f:
             f.write("uacomment=relative\n")
+        # - tmpdir/node0/relative2.conf
+        with open(os.path.join(self.options.tmpdir + "/node0", "relative2.conf"), "w", encoding="utf8") as f:
+            f.write("uacomment=relative2\n")
         with open(os.path.join(self.options.tmpdir + "/node0", "bitcoin.conf"), 'a', encoding='utf8') as f:
             f.write("uacomment=main\nincludeconf=relative.conf\n")
-        # subversion should end with "(main; relative)/"
 
     def set_test_params(self):
         self.setup_clean_chain = False
         self.num_nodes = 1
 
     def run_test(self):
-        nwinfo = self.nodes[0].getnetworkinfo()
-        subversion = nwinfo["subversion"]
+        self.log.info("-includeconf works from config file. subversion should end with 'main; relative)/'")
+
+        subversion = self.nodes[0].getnetworkinfo()["subversion"]
         assert subversion.endswith("main; relative)/")
+
+        self.log.info("-includeconf cannot be used as command-line arg. subversion should still end with 'main; relative)/'")
+        self.restart_node(0, extra_args=["-includeconf=relative.conf"])
+
+        subversion = self.nodes[0].getnetworkinfo()["subversion"]
+        assert subversion.endswith("main; relative)/")
+
+        self.log.info("-includeconf cannot be used recursively. subversion should with 'main)/'")
+        with open(os.path.join(self.options.tmpdir + "/node0", "relative.conf"), "w", encoding="utf8") as f:
+            f.write("includeconf=relative2.conf\n")
+
+        self.restart_node(0)
+
+        subversion = self.nodes[0].getnetworkinfo()["subversion"]
+        assert subversion.endswith("main)/")
+
+        self.log.info("multiple -includeconf args can be used from the base config file. subversion should with 'main)/'")
+        with open(os.path.join(self.options.tmpdir + "/node0", "relative.conf"), "w", encoding="utf8") as f:
+            f.write("uacomment=relative\n")
+        with open(os.path.join(self.options.tmpdir + "/node0", "bitcoin.conf"), 'a', encoding='utf8') as f:
+            f.write("uacomment=main\nincludeconf=relative.conf\nincludeconf=relative2.conf")
+
+        self.restart_node(0)
+
+        subversion = self.nodes[0].getnetworkinfo()["subversion"]
+        assert subversion.endswith("main)/")
 
 if __name__ == '__main__':
     IncludeConfTest().main()
