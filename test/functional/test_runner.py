b3944ef736982cddf3e71a03a1ee5c41d5377de4
Pieter Wuille <pieter.wuille@gmail.com>
2019-04-16 16:38:06 -0700

    [TESTS] Functional tests for Schnorr/Taproot/Tapscript
    Includes sighashing code and many tests by Johnson Lau.
    Includes a test by Matthew Zipkin.

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -152,6 +152,7 @@ BASE_SCRIPTS = [
     'p2p_filter.py',
     'rpc_setban.py',
     'p2p_blocksonly.py',
+    'feature_taproot.py',
     'mining_prioritisetransaction.py',
     'p2p_invalid_locator.py',
     'p2p_invalid_block.py',
