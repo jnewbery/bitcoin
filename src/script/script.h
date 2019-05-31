66207e86fe9a32bff98c944dc43c6678ccadc23b
John Newbery <john@johnnewbery.com>
2019-05-31 15:17:39 -0400

    OP_CODEs classic
    Restores OPCODEs disabled by Satoshi in 2010:
    
    - OP_CAT
    - OP_SUBSTR
    - OP_LEFT
    - OP_RIGHT
    - OP_INVERT
    - OP_AND
    - OP_OR
    - OP_XOR
    - OP_2MUL
    - OP_2DIV
    - OP_MUL
    - OP_DIV
    - OP_MOD
    - OP_LSHIFT
    - OP_RSHIFT
    
    Paritally reverts 6ac7f9f144757f5f1a049c059351b978f83d1476 and bce697d7faef0d8119ba80ae583cfcce460c7f81
    
    This was not a straight revert due to the changes made in PR 3965

--- src/script/script.h
+++ src/script/script.h
@@ -263,8 +263,17 @@ public:
 
     inline CScriptNum operator+(   const int64_t& rhs)    const { return CScriptNum(m_value + rhs);}
     inline CScriptNum operator-(   const int64_t& rhs)    const { return CScriptNum(m_value - rhs);}
+    inline CScriptNum operator*(   const int64_t& rhs)    const { return CScriptNum(m_value * rhs);}
+    inline CScriptNum operator/(   const int64_t& rhs)    const { return CScriptNum(m_value / rhs);}
+    inline CScriptNum operator%(   const int64_t& rhs)    const { return CScriptNum(m_value % rhs);}
     inline CScriptNum operator+(   const CScriptNum& rhs) const { return operator+(rhs.m_value);   }
     inline CScriptNum operator-(   const CScriptNum& rhs) const { return operator-(rhs.m_value);   }
+    inline CScriptNum operator*(   const CScriptNum& rhs) const { return operator*(rhs.m_value);   }
+    inline CScriptNum operator/(   const CScriptNum& rhs) const { return operator/(rhs.m_value);   }
+    inline CScriptNum operator%(   const CScriptNum& rhs) const { return operator%(rhs.m_value);   }
+
+    inline CScriptNum operator<<=(   const int64_t& rhs)    { return CScriptNum(m_value << rhs);}
+    inline CScriptNum operator>>=(   const int64_t& rhs)    { return CScriptNum(m_value >> rhs);}
 
     inline CScriptNum& operator+=( const CScriptNum& rhs)       { return operator+=(rhs.m_value);  }
     inline CScriptNum& operator-=( const CScriptNum& rhs)       { return operator-=(rhs.m_value);  }
