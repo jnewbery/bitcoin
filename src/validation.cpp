37458fdd1033cb30ec0591d0932e7d6a6b0dee32
Anthony Towns <aj@erisian.com.au>
2021-04-16 18:34:34 +1000

    [refactor] Move ComputeBlockVersion into VersionBitsCache
    This also changes ComputeBlockVersion to take the versionbits cache
    mutex once, rather than once for each versionbits deployment.

--- src/validation.cpp
+++ src/validation.cpp
@@ -1702,7 +1702,7 @@ public:
         return pindex->nHeight >= params.MinBIP9WarningHeight &&
                ((pindex->nVersion & VERSIONBITS_TOP_MASK) == VERSIONBITS_TOP_BITS) &&
                ((pindex->nVersion >> bit) & 1) != 0 &&
-               ((ComputeBlockVersion(pindex->pprev, params) >> bit) & 1) == 0;
+               ((versionbitscache.ComputeBlockVersion(pindex->pprev, params) >> bit) & 1) == 0;
     }
 };
 
